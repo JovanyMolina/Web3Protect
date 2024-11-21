b83bdf0a68010bcd05c4f60ecea88a4a8b7362ac
JovanyMolina <jovanymolina822@gmail.com>
2024-11-20 21:45:55 -0600

    se coloco diseño en RegistrarAdmin.php y RegistrarUsuario.php

--- RegistrarAdmin.php
+++ RegistrarAdmin.php
@@ -7,18 +7,19 @@
     <link rel="stylesheet" href="Estilos/EstilosRegistroAdmin.css">
 </head>
 <body>
+
     <div class="form-container">
+        <form action="paginas/AdminProceso.php" method="POST" class="box">
         <h2>Registrar Administrador</h2>
-        <form action="paginas/AdminProceso.php" method="POST">
             <label for="usuario">Usuario:</label>
             <input type="text" id="usuario" name="usuario" required>
 
             <label for="password">Contraseña:</label>
             <input type="password" id="password" name="password" required>
             
-            <input type="submit" value="Registrar">
-            <button type="button" onclick="history.back()">Regresar</button>
-
+            <input type="submit" value="Registrar" id="registrar">
+<!-- <button type="button" onclick="history.back()">Regresar</button>
+-->
         </form>
     </div>
 </body>
--- RegistrarUsuario.php
+++ RegistrarUsuario.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro Usuario</title>
-    <link rel="stylesheet" href="Estilos/EstilosRegistroUsuario.css">
+    <link rel="stylesheet" href="Estilos/EstilosRegistroUsuarios.css">
 </head>
 <body>
     <div class="formu">
-        <form action="paginas/RegistroUsuaruiosProceso.php" method="POST">
+        <form action="paginas/RegistroUsuaruiosProceso.php" method="POST" class="box">
             <h2>Registro de usuario</h2>
             <label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" required>
@@ -21,7 +21,7 @@
             <label type="text">Maestria:</label>
             <input type="text" id="maestria" name="maestria" required>
 
-            <button type="submit">Registrar</button>
+            <button type="submit" id="registrar">Registrar</button>
         </form>
     </div>
 
