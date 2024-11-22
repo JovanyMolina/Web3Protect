1d9bd2c2c9608e78d1258bc379e651c89237fb11
JovanyMolina <s20120208@alumnos.itsur.edu.mx>
2024-11-21 19:38:54 -0600

    tabla id

--- paginas/PanelProceso.php
+++ paginas/PanelProceso.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/../Conexion/ConnecionMySql.php';
 $conn = new ConnectionMySQL();
 $conn->CreateConnection();
 
-$sql = "SELECT (@row_number := @row_number + 1) AS id,Nombre,Apellidos,Telefono,CedulaProf,Maestría 
-        FROM usuarios, (SELECT @row_number := 0) AS t";
+$sql = "SELECT id,Nombre,Apellidos,Telefono,CedulaProf,Maestría 
+        FROM usuarios";
 $result = $conn->ExecuteQuery($sql);
 
 ?>
